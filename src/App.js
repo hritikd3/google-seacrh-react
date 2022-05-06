5be505084580cd95cac247535fd737264eadc040
hritik <86347286+hritikd3@users.noreply.github.com>
2022-05-06 22:33:36 +0530

    initial testing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import "./styles.css";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2> editing to see some magic happen!</h2>
 
-<Router >
-<Routes> 
-  <Route path="/" element="" />
-</Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element="" />
+        </Routes>
 
-<Routes>  
-  <Route path='/home' element="" />
-</Routes>
-  </Router>
-
+        <Routes>
+          <Route path="/home" element="" />
+        </Routes>
+      </Router>
     </div>
   );
 }
