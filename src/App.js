ef5064498bb24194451c71a1abbea3ad03723296
hritik <86347286+hritikd3@users.noreply.github.com>
2022-05-06 22:33:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+
+<Router >
+<Routes> 
+  <Route path="/" element="" />
+</Routes>
+
+<Routes>  
+  <Route path='/home' element="" />
+</Routes>
+  </Router>
+
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default Headers(){
+return (
+  <header>
+  <div> 
+  <a href="" alt="">Home </a>
+  <a href="" alt="" />
+  
+  </div>
+  </header>
+
+)
+}
