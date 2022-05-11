d86993dc2f161b54f43a82099fb7100acad503a8
hritik <86347286+hritikd3@users.noreply.github.com>
2022-05-11 15:25:55 +0530

    header file imported

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./styles.css";
+import { Header } from "./components";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2> editing to see some magic happen!</h2>
-
+   
       <Router>
         <Routes>
           <Route path="/" element="" />
